4c2bd03a468c2f17f3756aaa387bb7eb31d80381
luis <desarollador.web.tuxtla@gmail.com>
2021-05-02 17:55:35 -0500

    comprimir js y unirlos

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -53,38 +53,4 @@ $("#name").focus(function () {
 });
 
 
-
-
-
-
-
-/*
-$(function () { $("#contactForm input,#contactForm textarea,#contactForm button").not("[type=button]").jqBootstrapValidation(); } );
-
-$( "#sendMessageButton" ).click(function() {
-
-    $(function () { $("#contactForm input,#contactForm textarea,#contactForm button").not("[type=button]").jqBootstrapValidation(); } );
-/*
-    if( $("#name").val() === ""){
-        console.log("vacio");
-    }else{
-        console.log("tiene algo");
-    }
-
-    // Success message
- $("#success").html("<div class='alert alert-success'>");
- $("#success > .alert-success")
-     .html(
-         "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
-     )
-     .append("</button>");
- $("#success > .alert-success").append(
-     "<strong>Your message has been sent. </strong>"
- );
- $("#success > .alert-success").append("</div>");
- //clear all fields
- $("#contactForm").trigger("reset");
-*/
-/*});*/
- 
  
